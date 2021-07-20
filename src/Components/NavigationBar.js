c6fb6cc9d782917848da6c480cf02739eca04acc
jasonma <jasonma1127@gmail.com>
2021-07-21 00:59:53 +0800

    1. Adding RWD on navigation bar 2. Adding RWD on home page (including img, bar, title, p, button)

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function NavigationBar(props) {
   return (
@@ -10,26 +12,48 @@ function NavigationBar(props) {
       <div className="nav-btn">
         <ul className="nav-items">
           <li className="nav-item">
-            <NavLink to={process.env.PUBLIC_URL + "/"} exact activeClassName="active">
+            <NavLink
+              to={process.env.PUBLIC_URL + "/"}
+              exact
+              activeClassName="active"
+            >
               Home
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to={process.env.PUBLIC_URL + "/about"} exact activeClassName="active">
+            <NavLink
+              to={process.env.PUBLIC_URL + "/about"}
+              exact
+              activeClassName="active"
+            >
               About
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to={process.env.PUBLIC_URL + "/portfolios"} exact activeClassName="active">
+            <NavLink
+              to={process.env.PUBLIC_URL + "/portfolios"}
+              exact
+              activeClassName="active"
+            >
               Portfolios
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to={process.env.PUBLIC_URL + "/contact"} exact activeClassName="active">
+            <NavLink
+              to={process.env.PUBLIC_URL + "/contact"}
+              exact
+              activeClassName="active"
+            >
               Contact
             </NavLink>
           </li>
         </ul>
+        <span className="nav-item-bars pad">
+          <FontAwesomeIcon icon={faBars} className="fa-3x"/>
+        </span>
+        <span className="nav-item-bars mobile">
+          <FontAwesomeIcon icon={faBars} className="fa-1x"/>
+        </span>
       </div>
     </div>
   );
