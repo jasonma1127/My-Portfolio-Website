c610d62a38df7a8ec95327100bd42389042d34ec
jasonma <jasonma1127@gmail.com>
2021-07-26 14:35:07 +0800

    1. Fix portfolios page

--- src/Components/PortfoliosCard.js
+++ src/Components/PortfoliosCard.js
@@ -3,16 +3,12 @@ import React from "react";
 function PortfoliosCard(props) {
   return (
     <div className="portfolios-card">
-      <div className="portfolios-card-inner">
-        <div className="portfolios-card-front">
-          <div className="portfolios-content">
-            <h2 className="portfolios-title">{props.title}</h2>
-            <p>{props.description}</p>
-          </div>
-        </div>
-        <div className="portfolios-card-back">
-          <button>Check It Out</button>
-        </div>
+      <div className="portfolios-content">
+        <h2 className="portfolios-title">{props.title}</h2>
+        <p>{props.description}</p>
+      </div>
+      <div className="portfolios-btn">
+        <button>Check It Out</button>
       </div>
     </div>
   );
